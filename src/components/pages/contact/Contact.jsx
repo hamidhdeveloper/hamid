902f2153f799a86d4e2aca2d5b107ff61157bbe9
Hamid Hussain <hamidhussain740@gmail.com>
2023-05-15 11:36:36 +0300

    hi

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,6 +1,92 @@
-import React from "react";
+import React,{useState} from "react";
+import ReCAPTCHA from 'react-google-recaptcha';
+
 
 const Contact = () => {
+  // for recaptcha
+  // eslint-disable-next-line
+  const [captchaValue, setCaptchaValue] = useState('');
+  const handleCaptchaChange = (value) => {
+    setCaptchaValue(value);
+  };
+
+
+  // contact form starts
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [messageError, setMessageError] = useState('');
+
+  // validate on blur function
+const validateEmail = () => {
+  if (!email) {
+    setEmailError('Email is required');
+    return false;
+  } else if (!/\S+@\S+\.\S+/.test(email)) {
+    setEmailError('Email is invalid');
+    return false;
+  } else {
+    setEmailError('');
+    return true;
+  }
+};
+const validateMessage = () => {
+  if (!message) {
+    setMessageError('Project details required');
+    return false;
+  } else if (message.length < 20) {
+    setMessageError('At least 20 characters long');
+    return false;
+  } else {
+    setMessageError('');
+    return true;
+  }
+};
+
+
+const handleSubmit = async(e) => {
+  e.preventDefault();
+  let formIsValid = true;
+  
+// validate email
+if (!email) {
+  setEmailError('Email is required');
+  formIsValid = false;
+} else if (!/\S+@\S+\.\S+/.test(email)) {
+  setEmailError('Email is invalid');
+  formIsValid = false;
+} else {
+  setEmailError('');
+}
+
+// validate message
+if (!message) {
+  setMessageError('Project details required');
+  formIsValid = false;
+} else if (message.length < 20) {
+  setMessageError('At least 20 characters long');
+  formIsValid = false;
+} else {
+  setMessageError('');
+}
+
+
+   
+// submit the form if there are no errors
+    if (formIsValid) {
+
+      
+      // const data = {
+      //   email: email,
+      //   message: message
+      // };
+      // api here
+    }
+
+}
+  // contact form ends
   return (
     <>
       <div
@@ -15,14 +101,13 @@ const Contact = () => {
                 <div className="form-box">
                   <h2>Let's talk?</h2>
                   <p>
-                    It's all about the humans behind a brand and those
-                    experiencing it, br we're right there. In the middle
-                    performance quick.
+                  I'm always here to chat and help answer any questions you might have about Web development. So, what's on your mind today?
+
+
                   </p>
                   <form
-                    action="assets/mail/contact.php"
-                    method="POST"
                     className="contact-form"
+                    onSubmit={handleSubmit}
                   >
                     <div className="row">
                       <div className="col-lg-12">
@@ -33,8 +118,9 @@ const Contact = () => {
                             name="name"
                             placeholder="Name"
                             type="text"
+                            value={name} onChange={(e) => setName(e.target.value)} 
                           />
-                          <span className="alert-error" />
+                          <span className="alert-error" style={{color: '#FF004F'}}></span>
                         </div>
                       </div>
                     </div>
@@ -47,8 +133,9 @@ const Contact = () => {
                             name="email"
                             placeholder="Email*"
                             type="email"
+                            value={email} onChange={(e) => setEmail(e.target.value)} onBlur={validateEmail}
                           />
-                          <span className="alert-error" />
+                          <span className="alert-error" style={{color: '#FF004F'}}>{emailError}</span>
                         </div>
                       </div>
                       <div className="col-lg-6">
@@ -59,6 +146,7 @@ const Contact = () => {
                             name="phone"
                             placeholder="Phone"
                             type="text"
+                            value={phone} onChange={(e) => setPhone(e.target.value)} 
                           />
                           <span className="alert-error" />
                         </div>
@@ -72,21 +160,23 @@ const Contact = () => {
                             id="comments"
                             name="comments"
                             placeholder="Tell Us About Project *"
-                            defaultValue={""}
+                            value={email} onChange={(e) => setMessage(e.target.value)} onBlur={validateMessage}
                           />
+                          <span className="alert-error" style={{color: '#FF004F'}}>{messageError}</span>
                         </div>
                       </div>
                     </div>
                     <div className="row">
                       <div className="col-lg-12">
-                        <button type="submit" name="submit" id="submit">
+                      <ReCAPTCHA sitekey="6LepXA4mAAAAAMZCqxVtG4UCQfZ0p3hAxXLZWI4S" onChange={handleCaptchaChange} />
+                        <button type="submit" name="submit" >
                           Send Message
                         </button>
                       </div>
                     </div>
                     {/* Alert Message */}
                     <div className="col-lg-12 alert-notification">
-                      <div id="message" className="alert-msg" />
+                      <div id="message" className="alert-msg" ></div>
                     </div>
                   </form>
                 </div>
@@ -99,15 +189,12 @@ const Contact = () => {
                         <i className="fas fa-envelope-open-text" />
                       </div>
                       <div className="info">
-                        <p>Our Email</p>
+                        <p>My Email</p>
                         <h5>
-                          <a href="mailto:info@validtheme.com">
-                            info@validtheme.com
+                          <a href="mailto:hamidhussain740@gmail.com">
+                            hamidhussain740@gmail.com
                           </a>
-                          <br />
-                          <a href="mailto:support@softar.com">
-                            support@softar.com
-                          </a>
+                          
                         </h5>
                       </div>
                     </li>
@@ -121,8 +208,7 @@ const Contact = () => {
                       <div className="info">
                         <p>Address</p>
                         <h5>
-                          22 Baker Street, <br /> London, United Kingdom, <br />{" "}
-                          W1U 3BW
+                        Al Olaya Riyadh, <br /> Saudi Arabia (KSA) 
                         </h5>
                       </div>
                     </li>
@@ -136,9 +222,7 @@ const Contact = () => {
                       <div className="info">
                         <p>Phone</p>
                         <h5>
-                          <a href="tel:+4733378901">+44-20-7328-4499</a>
-                          <br />
-                          <a href="tel:+4733378901">+99-34-8878-9989</a>
+                          <a href="tel:+966575568052">+966-575-568-052</a>
                         </h5>
                       </div>
                     </li>
