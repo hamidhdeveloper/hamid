297abb3e7dfaf8df5f62fd01d054d3a449b51771
Hamid Hussain <hamidhussain740@gmail.com>
2023-06-13 17:57:04 +0300

    cv updated preloader added

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,50 +1,13 @@
 import React from "react";
-
+import styles from "./Loading.module.css";
 const Loading = ({ showLoading }) => {
   return (
     <>
     {showLoading && (
       <>
-      <div id="preloader">
-        <div id="ambrox-preloader" className="ambrox-preloader">
-          <div className="animation-preloader">
-            <div className="spinner" />
-            <div className="txt-loading">
-              <span data-text-preloader="H" className="letters-loading">
-                H
-              </span>
-              <span data-text-preloader="A" className="letters-loading">
-                A
-              </span>
-              <span data-text-preloader="M" className="letters-loading">
-                M
-              </span>
-              <span data-text-preloader="I" className="letters-loading">
-                I
-              </span>
-              <span data-text-preloader="D" className="letters-loading">
-                D
-              </span>
-              
-            </div>
-          </div>
-          <div className="loader">
-            <div className="row">
-              <div className="col-3 loader-section section-left">
-                <div className="bg" />
-              </div>
-              <div className="col-3 loader-section section-left">
-                <div className="bg" />
-              </div>
-              <div className="col-3 loader-section section-right">
-                <div className="bg" />
-              </div>
-              <div className="col-3 loader-section section-right">
-                <div className="bg" />
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className={styles.preloader}>
+        <span>{'{'}</span>
+        <span>{'}'}</span>
       </div>
       </>
       )}
