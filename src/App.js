813252796e3594228def1a9fae92d3bcc468e57f
Hamid Hussain <hamidhussain740@gmail.com>
2023-07-13 20:19:14 +0300

    new changes done loading reomved

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Home from './components/pages/home/Home';
-import Loading from './components/loading/Loading';
 
 function App() {
   
-  const [showLoading, setshowLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate the loading process
-    setTimeout(() => {
-      setshowLoading(false);
-    }, 3000);
-  }, []);
 
   return (
     <div className="App">
-      {showLoading ? (
-        <Loading showLoading={showLoading} />
-      ) : (
-        // Your main content goes here
+  
         <Home />
-      )}
       
     </div>
   );
